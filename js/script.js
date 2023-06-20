d496ce44a575a1659943af7bca21039d1a2cebc8
aswati94 <aswatistudy@gmail.com>
2023-06-20 13:06:41 +0530

    js-function

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// create a function
+// Function <functionName>(){function body} ---> Function Syntax
+// 1. Function definition is a one time process
+function  greetMyStudents(name){    // I will recieve the formal arguments
+    console.log('hello');
+
+}
+// 2. Function calling/Invocation Call/Invoke  is a many time calling
+greetMyStudents("Abhi");   //we can pass some actual arguments
+greetMyStudents('Jyoti');
+greetMyStudents(`Varsha`);
+greetMyStudents("Geetika");
+greetMyStudents('Purva');
+greetMyStudents('Samarth');
---
+++ js/script2.js
@@ -0,0 +1,12 @@
+//Add two numbers using console.log (Repeat Yourself concept)
+console.log(10+20);
+console.log(20+30);
+console.log(40+60);
+
+//Add two numbers using function(Don't repeat Yourself concept)
+// Add two numbers using function now
+function addTwoNumbers(n1,n2){
+    console.log(n1+n2);
+}
+addTwoNumbers(10,20);
+addTwoNumbers(30,80);
